6bc96a1d1621833cdc58c9ca464cf3ffd41776a7
Milad Dehghan <milawddehghan@gmail.com>
2021-01-17 19:21:38 +0330

    2-2-02

--- 2. Templates/2. Templating with concatenation/02/main.go
+++ 2. Templates/2. Templating with concatenation/02/main.go
@@ -1,1 +1,36 @@
-package _2
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main()  {
+	name := "Sara"
+	tpl := fmt.Sprint(`
+			<!DOCTYPE html>
+			<html>
+			<head>
+			<title>Page Title</title>
+			</head>
+			<body>
+			<h6>my name is:`+ name +`</h6>
+			</body>
+			</html> 
+			`)
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Println(err)
+	}
+	defer nf.Close()
+
+	io.Copy(nf, strings.NewReader(tpl))
+}
+/*
+
+	go run main.go
+
+*/
